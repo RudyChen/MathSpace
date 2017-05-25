5dd0c9b98953aa034e8a42cd790a53fa46ac885d
RudyChen <1245470512@qq.com>
2017-05-26 01:48:27 +0800

    添加组合对象

---
+++ Model/IBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSpace.Model
+{
+   public interface IBlock
+    {
+        double GetHeight();
+
+        double GetVerticalAlignmentCenter();
+
+        /// <summary>
+        /// 给每个元素计算一个位置
+        /// </summary>
+        void AlignElementsToVerticalCenter();
+    }
+}
